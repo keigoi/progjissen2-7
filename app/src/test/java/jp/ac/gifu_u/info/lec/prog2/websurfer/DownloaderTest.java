6cc1719ccebb866983ec345bc62fb92cab5010ae
keigoi <keigoi@gifu-u.ac.jp>
2017-05-31 03:33:19 +0900

    岐阜大学内のプロキシ設定の追加

--- app/src/test/java/jp/ac/gifu_u/info/lec/prog2/websurfer/DownloaderTest.java
+++ app/src/test/java/jp/ac/gifu_u/info/lec/prog2/websurfer/DownloaderTest.java
@@ -2,7 +2,11 @@ package jp.ac.gifu_u.info.lec.prog2.websurfer;
 
 import junit.framework.TestCase;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,6 +15,15 @@ import static org.junit.Assert.*;
  */
 public class DownloaderTest {
 
+    /**
+     * プロキシ設定（岐阜大学内専用...）
+     */
+    @Before
+    public void setUp() {
+        Properties systemProperties = System.getProperties();
+        // systemProperties.setProperty("http.proxyHost", "proxy.yanagi.gifu-u.ac.jp");
+        // systemProperties.setProperty("http.proxyPort", "10080");
+    }
     /*
      * 何も前提がなければダウンロードが成功することを確認するテスト
      */
@@ -31,6 +44,9 @@ public class DownloaderTest {
         Downloader d = new Downloader();
 
         d.addBannedDomain("example.com");
+
+        assertFalse( d.download("http://example.com/index.html") );
+        assertNull( d.getContent() );
 
         assertFalse( d.download("http://bad.example.com/index.html") );
         assertNull( d.getContent() );
